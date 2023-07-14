d5061347cf0b8c7c6ca305b136658d537152ef0b
waska14 <waska@chaduneli.com>
2023-07-14 15:14:05 +0400

    Test

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,8 @@ import DeviceError from "@/components/DeviceError.vue";
 export default {
   data() {
     return {
-      initial_countdown: 2,
-      countdown: 2,
+      initial_countdown: 30,
+      countdown: 30,
     }
   },
   mounted() {
