8a5ac2b4b6054bad2b2432553bbdfb049dcdc4af
waska14 <waska@chaduneli.com>
2023-10-09 16:10:08 +0400

    test

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="font-russo-one color-main title text-center">MyKeyBox</div>
+    <div class="font-russo-one color-main title text-center">Niko</div>
 </template>
 
 <style scoped>
